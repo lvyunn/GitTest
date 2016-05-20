87eea05abd2d3e9f610771d3113cf74566cd6c15
lvyunn <908226928@qq.com>
2016-05-20 15:53:58 +0800

    first version,add 1 files

---
+++ main.cpp
@@ -0,0 +1,17 @@
+/*
+ * 实例1 Hello World
+ **/
+
+#include <QApplication>
+#include <QPushButton>
+
+int main(int argc, char *argv[])
+{
+    QApplication app(argc, argv);
+    QPushButton b("Hello World!");
+    b.show();
+
+    QObject::connect(&b,SIGNAL(clicked()),&app,SLOT(quit()));
+
+    return app.exec();
+}
